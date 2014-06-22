0617d4c5382cb84ecf0220a5bcc52bcb4a2f12a5
bulton-fr <bulton.fr@gmail.com>
2014-06-22 16:16:00 +0200

    Ajout de la fonction DB_protect utilisé dans la fonction secure() du Kernel BFW.

---
+++ src/functions/functions.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Fonctions utile au module BFW_SQL
+ * @author Vermeulen Maxime <bulton.fr@gmail.com>
+ * @package bfw-sql
+ * @version 1.0
+ */
+
+/**
+ * Permet de protéger une donnée pour les requêtes
+ * 
+ * @param string $data : La donnée à protéger
+ * 
+ * @return string
+ */
+function DB_protect($data)
+{
+	if(!is_null($DB)) {return $DB->protect($data);}
+	else {return $data}
+}
