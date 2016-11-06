0707dea856a54b3b337791d58061777b21fa7f0b
bulton-fr <bulton.fr@gmail.com>
2016-11-06 19:26:14 +0100

    Add new module declaration for bfw v3.0

---
+++ src/runner.php
@@ -0,0 +1,30 @@
+<?php
+
+$moduleConfig      = $module->getConfig();
+$configListBases   = $moduleConfig->getConfig('bases');
+$configNbBases     = count($configListBases);
+$module->listBases = [];
+
+$observerConfig  = $moduleConfig->getConfig('observer');
+$observerLogFile = $observerConfig->logFile;
+
+if (
+    $observerConfig->enable === true
+    && !empty($observerLogFile)
+) {
+    $observer = new \BfwSql\SqlObserver($observerConfig);
+    \BFW\Application::getInstance()->attach($observer);
+}
+
+foreach ($configListBases as $baseInfos) {
+    $baseKey = $baseInfos->baseKeyName;
+
+    if (empty($baseKey) && $configNbBases > 1) {
+        throw new \Exception(
+            'bfw-sql : They are multiple connexion defined,'
+            .' a keyName must be define to each connexion.'
+        );
+    }
+    
+    $module->listBases[$baseKey] = new \BfwSql\SqlConnect($baseInfos);
+}
