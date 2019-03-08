baeea9a76217d83ba2eca537061a0fd4cb60d573
bulton-fr <bulton.fr@gmail.com>
2019-03-08 22:46:35 +0100

    Fix run test : Add new folder creation, constant and namespace
    It's not a problem to have "/src/models" and "/src/modeles" because the second is created by the framework and will be removed by 3.0.0-rc11

--- src/runner.php
+++ src/runner.php
@@ -5,6 +5,12 @@ namespace BfwSql;
 $moduleConfig = $this->getConfig();
 $usedClass    = \BfwSql\UsedClass::getInstance($moduleConfig);
 
+\BFW\Helpers\Constants::create('MODELS_DIR', SRC_DIR.'models/');
+\BFW\Application::getInstance()
+    ->getComposerLoader()
+    ->addPsr4('Models\\', MODELS_DIR)
+;
+
 $runnerClasses = [
     $usedClass->obtainClassNameToUse('RunnerMonolog'),
     $usedClass->obtainClassNameToUse('RunnerObservers'),
